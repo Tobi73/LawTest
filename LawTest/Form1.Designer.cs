f43826b2833316a298cd6912099b3aa08aa38d99
Артем <arefiev_1997@mail.ru>
2017-11-29 22:25:21 +0400

    lk

--- LawTest/Form1.Designer.cs
+++ LawTest/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
@@ -35,13 +36,23 @@
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.answerIcon4 = new System.Windows.Forms.PictureBox();
+            this.answerIcon3 = new System.Windows.Forms.PictureBox();
+            this.answerIcon2 = new System.Windows.Forms.PictureBox();
+            this.answerIcon1 = new System.Windows.Forms.PictureBox();
+            this.tipLabel = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.answerIcon4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.answerIcon3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.answerIcon2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.answerIcon1)).BeginInit();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -49,20 +60,22 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 25);
-            this.label1.MaximumSize = new System.Drawing.Size(600, 0);
+            this.label1.Location = new System.Drawing.Point(27, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.MaximumSize = new System.Drawing.Size(800, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(46, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(23, 29);
-            this.radioButton1.MaximumSize = new System.Drawing.Size(400, 0);
+            this.radioButton1.Location = new System.Drawing.Point(54, 36);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.MaximumSize = new System.Drawing.Size(533, 0);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.Size = new System.Drawing.Size(110, 21);
             this.radioButton1.TabIndex = 1;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "radioButton1";
@@ -71,10 +84,11 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(23, 67);
-            this.radioButton2.MaximumSize = new System.Drawing.Size(400, 0);
+            this.radioButton2.Location = new System.Drawing.Point(54, 82);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton2.MaximumSize = new System.Drawing.Size(533, 0);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.Size = new System.Drawing.Size(110, 21);
             this.radioButton2.TabIndex = 2;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "radioButton2";
@@ -83,10 +97,11 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(23, 108);
-            this.radioButton3.MaximumSize = new System.Drawing.Size(400, 0);
+            this.radioButton3.Location = new System.Drawing.Point(54, 133);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton3.MaximumSize = new System.Drawing.Size(533, 0);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.Size = new System.Drawing.Size(110, 21);
             this.radioButton3.TabIndex = 3;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "radioButton3";
@@ -95,10 +110,11 @@
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(23, 147);
-            this.radioButton4.MaximumSize = new System.Drawing.Size(400, 0);
+            this.radioButton4.Location = new System.Drawing.Point(54, 181);
+            this.radioButton4.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton4.MaximumSize = new System.Drawing.Size(533, 0);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(85, 17);
+            this.radioButton4.Size = new System.Drawing.Size(110, 21);
             this.radioButton4.TabIndex = 4;
             this.radioButton4.TabStop = true;
             this.radioButton4.Text = "radioButton4";
@@ -106,23 +122,75 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.answerIcon4);
+            this.groupBox1.Controls.Add(this.answerIcon3);
+            this.groupBox1.Controls.Add(this.answerIcon2);
+            this.groupBox1.Controls.Add(this.answerIcon1);
+            this.groupBox1.Controls.Add(this.tipLabel);
             this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.radioButton1);
             this.groupBox1.Controls.Add(this.radioButton4);
             this.groupBox1.Controls.Add(this.radioButton3);
-            this.groupBox1.Location = new System.Drawing.Point(15, 91);
+            this.groupBox1.Location = new System.Drawing.Point(20, 112);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(481, 191);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(667, 248);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Варианты ответов";
             // 
+            // answerIcon4
+            // 
+            this.answerIcon4.Location = new System.Drawing.Point(7, 181);
+            this.answerIcon4.Name = "answerIcon4";
+            this.answerIcon4.Size = new System.Drawing.Size(36, 26);
+            this.answerIcon4.TabIndex = 9;
+            this.answerIcon4.TabStop = false;
+            // 
+            // answerIcon3
+            // 
+            this.answerIcon3.Location = new System.Drawing.Point(7, 133);
+            this.answerIcon3.Name = "answerIcon3";
+            this.answerIcon3.Size = new System.Drawing.Size(36, 31);
+            this.answerIcon3.TabIndex = 8;
+            this.answerIcon3.TabStop = false;
+            // 
+            // answerIcon2
+            // 
+            this.answerIcon2.Location = new System.Drawing.Point(7, 82);
+            this.answerIcon2.Name = "answerIcon2";
+            this.answerIcon2.Size = new System.Drawing.Size(36, 34);
+            this.answerIcon2.TabIndex = 7;
+            this.answerIcon2.TabStop = false;
+            // 
+            // answerIcon1
+            // 
+            this.answerIcon1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.answerIcon1.Location = new System.Drawing.Point(7, 26);
+            this.answerIcon1.Name = "answerIcon1";
+            this.answerIcon1.Size = new System.Drawing.Size(36, 31);
+            this.answerIcon1.TabIndex = 6;
+            this.answerIcon1.TabStop = false;
+            // 
+            // tipLabel
+            // 
+            this.tipLabel.AutoSize = true;
+            this.tipLabel.ForeColor = System.Drawing.Color.Teal;
+            this.tipLabel.Location = new System.Drawing.Point(7, 227);
+            this.tipLabel.Name = "tipLabel";
+            this.tipLabel.Size = new System.Drawing.Size(46, 17);
+            this.tipLabel.TabIndex = 5;
+            this.tipLabel.Text = "label4";
+            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Location = new System.Drawing.Point(15, 12);
+            this.groupBox2.Location = new System.Drawing.Point(20, 15);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(664, 73);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(885, 90);
             this.groupBox2.TabIndex = 7;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Вопрос";
@@ -131,18 +199,20 @@
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label2.Location = new System.Drawing.Point(12, 299);
+            this.label2.Location = new System.Drawing.Point(16, 368);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(279, 13);
+            this.label2.Size = new System.Drawing.Size(360, 17);
             this.label2.TabIndex = 8;
             this.label2.Text = "Выберите вариант ответа и нажмите кнопку \"Далее\"";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::LawTest.Properties.Resources.question1;
-            this.pictureBox1.Location = new System.Drawing.Point(521, 103);
+            this.pictureBox1.Location = new System.Drawing.Point(695, 127);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(152, 152);
+            this.pictureBox1.Size = new System.Drawing.Size(203, 187);
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
             // 
@@ -150,17 +220,19 @@
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label3.Location = new System.Drawing.Point(12, 322);
+            this.label3.Location = new System.Drawing.Point(16, 396);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(218, 13);
+            this.label3.Size = new System.Drawing.Size(275, 17);
             this.label3.TabIndex = 11;
             this.label3.Text = "Можно вернуться, нажав кнопку \"Назад\"";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(465, 299);
+            this.button2.Location = new System.Drawing.Point(620, 368);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(108, 36);
+            this.button2.Size = new System.Drawing.Size(144, 44);
             this.button2.TabIndex = 10;
             this.button2.Text = "Назад";
             this.button2.UseVisualStyleBackColor = true;
@@ -169,9 +241,10 @@
             // button1
             // 
             this.button1.AllowDrop = true;
-            this.button1.Location = new System.Drawing.Point(579, 299);
+            this.button1.Location = new System.Drawing.Point(772, 368);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 36);
+            this.button1.Size = new System.Drawing.Size(133, 44);
             this.button1.TabIndex = 5;
             this.button1.Text = "Далее";
             this.button1.UseVisualStyleBackColor = true;
@@ -179,9 +252,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(694, 351);
+            this.ClientSize = new System.Drawing.Size(923, 422);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
@@ -190,13 +263,18 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(710, 390);
-            this.MinimumSize = new System.Drawing.Size(710, 390);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(941, 469);
+            this.MinimumSize = new System.Drawing.Size(941, 469);
             this.Name = "Form1";
             this.Text = "Тестирование";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.answerIcon4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.answerIcon3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.answerIcon2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.answerIcon1)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -219,6 +297,12 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox answerIcon4;
+        private System.Windows.Forms.PictureBox answerIcon3;
+        private System.Windows.Forms.PictureBox answerIcon2;
+        private System.Windows.Forms.PictureBox answerIcon1;
+        private System.Windows.Forms.Label tipLabel;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- LawTest/Form1.cs
+++ LawTest/Form1.cs
@@ -15,10 +15,10 @@ namespace LawTest
         TestProcesser TestProcessor;
         int taskNumber;
         int font = 9;
-        DataTable dt = new DataTable();
         Student student;
         long StartTime;
-        Stopwatch Stopwatch;
+        Stopwatch Stopwatch ;
+        bool TestFinished = false;
 
         public Form1(TestUnit testUnit, int _font, string studentFio, string studentGroup)
         {
@@ -51,31 +51,46 @@ namespace LawTest
             groupBox2.Font = new Font(groupBox2.Font.FontFamily, font);
             button1.Font = new Font(button1.Font.FontFamily, font);
             button2.Font = new Font(button2.Font.FontFamily, font);
-
-            dt.Columns.Add("Вопрос");
-            dt.Columns.Add("Варианты ответов");
-            dt.Columns.Add("Правильный ответ");
-            dt.Columns.Add("Комментарий");
-            dt.Columns.Add("+");
         }
 
         private void setTaskSettings()
         {
             if (taskNumber < TestUnit.Tasks.Count)
             {
+                button1.Visible = true;
+
                 var currentTask = TestUnit.Tasks[taskNumber];
-                var chosenAnswer = Answers.AnswersList[taskNumber].ChosenAnswer;
+                int chosenAnswer = Answers.AnswersList[taskNumber].ChosenAnswer;
                 label1.Text = taskNumber + 1 + " " + currentTask.TaskDescription;
+
+                tipLabel.Visible = TestFinished;
 
                 // Установка текста вариантов ответа
                 for (int i = 0; i < currentTask.Choices.Count; i++)
                 {
                     GetButtonRef(i).Text = currentTask.Choices[i];
                     GetButtonRef(i).Visible = true;
+                    GetPictureBoxRef(i).Visible = false;
+                    if (TestFinished)
+                    {
+                        if (i == chosenAnswer)
+                        {
+                            GetPictureBoxRef(i).BackColor = Color.Red;
+                            GetPictureBoxRef(i).Visible = true;
+                        }
+                        if (i == currentTask.CorrectAnswer)
+                        {
+                            GetPictureBoxRef(i).BackColor = Color.Green;
+                            GetPictureBoxRef(i).Visible = true;
+                        }
+
+                    }
+                    
                 }
                 for (int i = currentTask.Choices.Count; i < 4; i++)
                 {
                     GetButtonRef(i).Visible = false;
+                    GetPictureBoxRef(i).Visible = false;
                 }
 
                 // Установка состояний ответов
@@ -106,54 +121,28 @@ namespace LawTest
                 var correctAnswers = TestProcessor.CalculateAnswers(Answers);
                 var incorrectAnswers = TestUnit.Tasks.Count - correctAnswers;
                 var tasksNum = TestUnit.Tasks.Count;
-                //MessageBox.Show($"Тестирование завершено \r\nВсего вопросов: {tasksNum} \r\nПравильно: {correctAnswers} ", "Информация");
                 TestProcessor.SaveResultToFile(student, TestUnit, Answers, Stopwatch.ElapsedMilliseconds - StartTime);
-
-
-                FormResult fr = new FormResult(dt, font, tasksNum, correctAnswers, TestProcessor.GetResult(Answers));
-                fr.ShowDialog();
-
-                Close();
+                TestFinished = true;
+                button1.Text = "Закончить";
+                taskNumber--;
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            if (TestFinished)
+            {
+                var mark = TestProcessor.GetResult(Answers);
+                var correctAnswers = TestProcessor.CalculateAnswers(Answers);
+                var incorrectAnswers = TestUnit.Tasks.Count - correctAnswers;
+                var tasksNum = TestUnit.Tasks.Count;
+                TestProcessor.SaveResultToFile(student, TestUnit, Answers, Stopwatch.ElapsedMilliseconds - StartTime);
+                Close();
+            }
             int answer = setAns();
             if (answer != -1)
             {
                 var currentTask = TestUnit.Tasks[taskNumber];
-                //результат, по которому красится
-                string flag = "";
-                if(currentTask.Choices[answer].ToString() == currentTask.Choices[currentTask.CorrectAnswer].ToString())
-                {
-                    flag = "+";
-                } else
-                {
-                    flag = "-";
-                }
-                //добавление вариантов
-                string answers = "";
-                for (int i = 0; i< currentTask.Choices.Count;i++) {
-                    if (currentTask.Choices[currentTask.CorrectAnswer].ToString() == currentTask.Choices[i].ToString())
-                    {
-                        answers += "+";
-                    }
-                    else
-                    {
-                        // answers += "\u2022";
-                        answers += "-";
-                    }
-                    answers += currentTask.Choices[i];
-                    if (i+1 != currentTask.Choices.Count)
-                    {
-                        answers += "\r\n";
-                    }
-                }
-                //dt.Rows.Add(label1.Text, currentTask.Choices[currentTask.CorrectAnswer], currentTask.Choices[answer], currentTask.Tip,flag);
-                dt.Rows.Add(label1.Text, answers, currentTask.Choices[answer], currentTask.Tip, flag);
-
                 Answers.EditAnswer(taskNumber, answer);
                 taskNumber += 1;
                 setTaskSettings();
@@ -180,6 +169,18 @@ namespace LawTest
             }
         }
 
+        private PictureBox GetPictureBoxRef(int index)
+        {
+            switch(index)
+            {
+                case 0: return answerIcon1;
+                case 1: return answerIcon2;
+                case 2: return answerIcon3;
+                case 3: return answerIcon4;
+                default: throw new Exception("Could not get reference to picture box");
+            }
+        }
+
         private int setAns()
         {
             for (int i = 0; i < TestUnit.Tasks[taskNumber].Choices.Count; i++)
@@ -197,8 +198,6 @@ namespace LawTest
                     Answers.EditAnswer(taskNumber, answer);
                 taskNumber--;
                 setTaskSettings();
-
-                dt.Rows.RemoveAt(dt.Rows.Count - 1);
 
             }
             else
